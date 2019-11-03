83c1e461ceb57504d07487a139ed0aa249090231
KengSupakorn <42772611+KengSupakorn@users.noreply.github.com>
2019-11-03 17:36:11 +0700

    fix

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ import Notifications from "@material-ui/icons/Notifications";
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.js";
 import UserProfile from "views/UserProfile/UserProfile.js";
-import TableList from "views/TableList/TableList.js";
+
 import Typography from "views/Typography/Typography.js";
 import Icons from "views/Icons/Icons.js";
 import Volunteers from "views/Voluteers";
